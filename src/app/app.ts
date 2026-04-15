021479269bb9f694a418210f3b777187c15606d2
Andrea Silveira <asilveira@infragistics.com>
2026-04-15 16:33:45 -0300

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { IgxLinearGaugeModule } from 'igniteui-angular-gauges';
+import { IgxRadialGaugeModule } from 'igniteui-angular-gauges';
+import { IgxBulletGraphModule } from 'igniteui-angular-gauges';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IgxLinearGaugeModule, IgxRadialGaugeModule, IgxBulletGraphModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('testing-gauges');
+  linearValue = 65;
+  radialValue = 75;
 }
